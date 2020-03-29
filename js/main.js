e50326537429cb8e0a08e5f817235542f052ad0a
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-03-29 13:54:59 +0200

    add-background + change styles ticket + add logo ticket

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ bottoneGenera.addEventListener('click',
         }
 
         // Controllo dei decimali
-        costoBiglietto = costoBiglietto.toFixed(2) + '€';
+        costoBiglietto = costoBiglietto.toFixed(2);
 
         // Numero random per la carrozza da 1 a 9
         var numCarrozza = Math.floor( Math.random() * 9) + 1;
